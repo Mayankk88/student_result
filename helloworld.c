2143d4d8f4efd2e896d6e53c0ce1ddc5befe7463
Mayankk88 <praveshpal663@gmail.com>
2024-10-14 22:18:57 +0530

    UpdateD helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,29 +2,31 @@
 
 int main()
 {
-    int subject1;
-    int subject2;
-    int subject3;
-    printf("enter your marks of subject1\n");
-    scanf("%d", &subject1);
-    printf("enter your marks of subject2\n");
-    scanf("%d", &subject2);
-    printf("enter your marks of subject3\n");
-    scanf("%d", &subject3);
-    printf("the marks are %d,%d,%d.\n", subject1, subject2, subject3);
+    float subject1, subject2, subject3;
+
+    printf("enter your marks of subject1:\n");
+    scanf("%f", &subject1);
+    printf("enter your marks of subject2:\n");
+    scanf("%f", &subject2);
+    printf("enter your marks of subject3:\n");
+    scanf("%f", &subject3);
+
+    printf("the marks are %.2f,%.2f,%.2f.\n", subject1, subject2, subject3);
+
     float total = subject1 + subject2 + subject3;
     float result = total / 3;
+    
     if (subject1 < 33 || subject2 < 33 || subject3 < 33)
     {
-        printf("you are failed due to less marks in individual subject(s)\n");
+        printf("you are failed due to less marks in individual subject(s).\n");
     }
-    if (result < 40)
+    else if (result < 40)
     {
         printf("you are failed due to less percentage\n");
     }
-    else if (result > 40)
+    else
     {
-        printf("you are passed! %d pecentage",result);
+        printf("you are passed! with %.2f pecentage", result);
     }
     return 0;
 }
